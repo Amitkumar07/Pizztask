5d3fd2ea19ad9e27a66b7483c90df1ef3701a177
aniruddha <aniruddh.birajdaar@shikshainfotech.com>
2014-09-13 20:55:46 +0530

    added UI

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,7 @@
 class Person < ActiveRecord::Base
   has_many :purchases
   has_many :franchises , through: :purchases
+  def self.search_person_list(franchise_id , pizza_type_id)
+    @person= self.where(:purchases=> {:pizza_type_id=>pizza_type_id , :franchise_id=>franchise_id}).includes(:purchases)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post '/admin/search' => "home#search"
+  get '/admin/search'=> "home#index"
+ get "/home/index"=>"home#index"
+  ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
